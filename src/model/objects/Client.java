d20e91afa17fb5da364a1f5ab3c830ae787f6756
Carlos Arturo Diaz <70271822+ArturoDiaz02@users.noreply.github.com>
2021-09-17 14:58:22 -0500

    Default  objects

---
+++ src/model/objects/Client.java
@@ -0,0 +1,46 @@
+package src.model.objects;
+import src.model.data_structure.*;
+
+/**
+ * Client
+ */
+public class Client {
+
+    private String name;
+    private int cc;
+    private DefaultStack<Games> gamesStack;
+
+    public Client(String name, int cc, DefaultStack<Games> gamesStack) {
+        this.name = name;
+        this.cc = cc;
+        this.gamesStack = gamesStack;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCc() {
+        return this.cc;
+    }
+
+    public void setCc(int cc) {
+        this.cc = cc;
+    }
+
+    public DefaultStack<Games> getGamesStack() {
+        return this.gamesStack;
+    }
+
+    public void setGamesStack(DefaultStack<Games> gamesStack) {
+        this.gamesStack = gamesStack;
+    }
+
+
+
+    
+}
---
+++ src/model/objects/Games.java
@@ -0,0 +1,43 @@
+package src.model.objects;
+
+/**
+ * Games
+ */
+public class Games {
+
+    private int gameCode;
+    private double gamePrice;
+    private int amount;
+
+    public Games(int gameCode, double gamePrice, int amount) {
+        this.gameCode = gameCode;
+        this.gamePrice = gamePrice;
+        this.amount = amount;
+    }
+
+    public int getGameCode() {
+        return this.gameCode;
+    }
+
+    public void setGameCode(int gameCode) {
+        this.gameCode = gameCode;
+    }
+
+    public double getGamePrice() {
+        return this.gamePrice;
+    }
+
+    public void setGamePrice(double gamePrice) {
+        this.gamePrice = gamePrice;
+    }
+
+    public int getAmount() {
+        return this.amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    
+}
